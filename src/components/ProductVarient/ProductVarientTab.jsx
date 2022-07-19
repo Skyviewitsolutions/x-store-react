44a63ea3fefce3952c9c2328c91083c09feae920
Amit78-ux <amitdivyan1998@gmail.com>
2022-07-19 18:52:31 +0530

    Table

--- src/components/ProductVarient/ProductVarientTab.jsx
+++ src/components/ProductVarient/ProductVarientTab.jsx
@@ -5,10 +5,14 @@ import './ProductTab.css'
 const ProductVarientTab = () => {
     
     const columns = [
-        { label: 'Title', name: 'title' },
-        { label: 'Author', name: 'authors' },
-        { label: 'Page Count', name: 'num_pages', options: { sort: true } },
-        { label: 'Rating', name: 'rating' }
+        { label: 'Internal References', name: 'title' },
+        { label: 'Name', name: 'authors' },
+        { label: 'Attribute Value', name: 'num_pages', options: { sort: true } },
+        { label: 'Sales Price', name: 'rating' },
+        { label: 'Cast', name: 'rating' },
+        { label: 'Quantity Of Hand', name: 'rating' },
+        { label: 'Forcost Quantity', name: 'rating' },
+        { label: 'Unit Of Measers', name: 'rating' },
       ];
 
       const options = {
