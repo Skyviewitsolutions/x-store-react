900291efe3ac65adf5b7b8d384a7570c2dd32223
Anu3aug <priyapandey811507@gmail.com>
2022-08-23 16:34:37 +0530

    api

--- src/components/InventoryProductDetailsMain/InventoryDetailsEdit/AccountingEdit.jsx
+++ src/components/InventoryProductDetailsMain/InventoryDetailsEdit/AccountingEdit.jsx
@@ -6,7 +6,10 @@ import axios from 'axios';
 const AccountingEdit = () => {
 
   const incomeaccUrl = "https://xstore.skyviewads.com/AccountDepartment/ProductIncome/GetAllPI";
+  const expenseUrl = "https://xstore.skyviewads.com/AccountDepartment/ExpenseAccount/GetAllExp";
   const [income , setIncome] = useState([])
+  const [expense , setExpense] = useState([])
+
   useEffect(() => {
    axios.post(incomeaccUrl)
    .then((res) => {
@@ -23,7 +26,28 @@ const AccountingEdit = () => {
    .catch((err) => {
     console.log(err,'error');
    })
+
+
+
+   axios.post(expenseUrl)
+   .then((res) => {
+     console.log(res,'this is expense');
+     if(res.data.status == true) 
+     {
+       setExpense(res.data.data);
+     }
+     else if(res.data.status == false)
+     {
+       alert(res.data.message);
+     }
+   })
+   .catch((err) => {
+     console.log(err,'error');
+   })
   },[])
+
+
+ 
   return (
     <div className='AccountingContainer'>
             <div className="acc1">
@@ -48,16 +72,14 @@ const AccountingEdit = () => {
             <div className="AccountingEditcontent">
             <p>Expense Account</p>
             <select>
-                <option></option>
-              <option>110306001 ضريبة القيمة المضافة على المشتريات</option>
-              <option>110601001 مخزون قطع غيار</option>
-              <option>110601002 مخزون كفرات</option>
-              <option>110601003 مخزون زيوت وشحوم</option>
-              <option>110601004 مخزون ديزل </option>
-              <option>110601005 مخزون مواد جاهزة</option>
-              <option>110601006 وسيط مخزون</option>
-              <option><span  style={{ color: "#008784" }}>Search More....</span></option>
-              <option><span  style={{ color: "#008784" }}>Create and Edit...</span></option>
+              {expense.map((item,index) => {
+                return(
+                  <>
+                  <option value="110306001" key={index}>{item.EXPENSE_NAME}</option>
+                  </>
+                )
+              })}
+    
             </select>
            </div>
           
